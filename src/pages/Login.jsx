1bcb0da94b3e3850c7aa11ad6e52708d7036f7b9
Vivek <vivek66anand66@gmail.com>
2025-07-15 10:49:01 +0530

    some image path issue fixed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,13 @@
-// import loginImg from "../assets/Images/signup.webp"
+import loginImg from "../assets/images/frame.png"
 import Template from "../components/core/Auth/Template"
 
 function Login() {
     return (
         <Template 
             title = "Welcome Back"
-            // decription1 = "Build skills for today, tomorrow, and beyond."
+            // description1 = "Build skills for today, tomorrow, and beyond."
             // description2 = "Education to future-proof your career."
-            // image={loginImg}
+            image={loginImg}
             formType = "login"
         />
     )
